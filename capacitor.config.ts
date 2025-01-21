05bb803733ba52d32d4d3eda24bfb6eb895b8dcb
Wasim Akram <mr.saim516@gmail.com>
2025-01-21 17:25:52 +0500

    update / added apk file

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.oqgn3a.apk',
+  appName: 'OQGN 3a',
+  webDir: 'build'
+};
+
+export default config;
